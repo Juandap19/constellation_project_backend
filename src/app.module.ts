722b07d49b60c7236a3c3c3460a6e74495a399ac
Luis Pinillos <pinillosluisfer@gmail.com>
2024-10-02 22:56:52 -0500

    chore: connection to database established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }
+    ),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD, 
+      port: +process.env.DB_PORT,
+      autoLoadEntities: true,
+      synchronize: false, // solo para desarrollo
+      ssl: {
+        rejectUnauthorized: false, // Permite conexiones sin verificar el certificado, útil en desarrollo
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
