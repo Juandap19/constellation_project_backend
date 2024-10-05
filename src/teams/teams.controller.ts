4c7da33f60d028ac34aae0e76a5fc21c0962f04c
Danielms111 <103274916+Danielms111@users.noreply.github.com>
2024-10-05 18:33:42 -0500

    feat: Adding team CRUD

---
+++ src/teams/teams.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TeamsService } from './teams.service';
+import { CreateTeamsDto } from './dto/create-teams.dto';
+
+@Controller('team')
+export class TeamsController {
+    constructor(private readonly teamsService: TeamsService) {}
+
+    @Get()
+    async findAll() {
+        return this.teamsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return this.teamsService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() createTeamsDto: CreateTeamsDto) {
+        return this.teamsService.create(createTeamsDto);
+    }
+
+    @Put(':id')
+    async update (
+        @Param('id') id: string,
+        @Body() updateTeamsDto: CreateTeamsDto
+    ) {
+        return this.teamsService.update(id, updateTeamsDto);
+    }
+    
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        return this.teamsService.remove(id);
+    }
+}
