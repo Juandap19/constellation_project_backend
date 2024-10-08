41e76029423c9f8f6af3b362f872ac1f2a376841
Luis Pinillos <102693827+lpinillos@users.noreply.github.com>
2024-10-08 18:16:11 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Constellation example APIS')
-    .setDescription('The constellations API description, this project is a simple backend about creations of courses with activities, rubrics, criteria, and grades')
-    .setVersion('1.0')
-    .addTag('Constellations')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Constellation example APIS')
+  //   .setDescription('The constellations API description, this project is a simple backend about creations of courses with activities, rubrics, criteria, and grades')
+  //   .setVersion('1.0')
+  //   .addTag('Constellations')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
